658d5c8337baf235d3e805b0e25291b5c7b03030
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2025-03-10 14:55:16 -0700

    Feat: store and increment hits

--- src/ship.js
+++ src/ship.js
@@ -1,6 +1,11 @@
 class Ship {
     constructor(length){
         this.length = length;
+        this.hits = 0;
+    }
+
+    hit(){
+        this.hits++;
     }
 }
 
--- src/ship.test.js
+++ src/ship.test.js
@@ -4,10 +4,22 @@
 // Test EXPECTATION of sending with mocks of outgoing commands
 // test nothing else
 
+// and only test public interfaces, not private (with self) ones
+
 import {Ship} from "./ship.js"
 
 const testShip = new Ship(3);
 
 test('ship has length of constructor input', () => {
     expect(testShip.length).toBe(3);
+});
+
+test('ship hit property returns hits', () => {
+    expect(testShip.hits).toBe(0);
+})
+
+test('hit function increments hits', () => {
+    testShip.hit();
+    testShip.hit();
+    expect(testShip.hits).toBe(2);
 });
